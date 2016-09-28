e4b0be0af006fc24f046fb1239bf1b992a565f88
nokaa <nokaa@cock.li>
2016-09-28 14:20:00 -0500

    Basic reading of OSB API

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Flippers
 Fun with the Old School Runescape Grand Exchange.
+
+Example usage:
+```go
+package main
+
+import (
+	"github.com/nokaa/flippers"
+	"log"
+)
+
+func main() {
+	item, err := GetItem("1519")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(item)
+}
+```
--- flippers.go
+++ flippers.go
@@ -1,7 +1,47 @@
-package main
+package flippers
 
-import "log"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
-func main() {
-	log.Println("Hello, world!")
+// The base url for the OSBuddy API.
+var BaseUrl = "https://api.rsbuddy.com/grandExchange?a=guidePrice&i="
+
+// TODO: Since golang has a weak type system, it may not be worth
+// attempting to enforce type constraints in this situation.
+type ItemId string
+
+// A struct for storing data related to an item on the Grand
+// Exchange. This struct matches with the JSON read from the
+// OSBuddy API.
+type Item struct {
+	Overall         int
+	Buying          int
+	BuyingQuantity  int
+	Selling         int
+	SellingQuantity int
+}
+
+// Reads the Grand Exchange information for the item with id `id` into
+// an `Item` struct.
+func GetItem(id ItemId) (Item, error) {
+	// Connect to the api
+	resp, err := http.Get(baseUrl + string(id))
+	if err != nil {
+		return Item{}, err
+	}
+	defer resp.Body.Close()
+
+	// Read all data from the api into `body`
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Item{}, err
+	}
+
+	// Parse the JSON read from the API into the `Item` struct
+	var item Item
+	err = json.Unmarshal(body, &item)
+	return item, err
 }
